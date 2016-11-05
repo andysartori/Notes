81afcaeccc1622d4725cb07a44660c15c7f9d48d
Andy Sartori <Andy@AndyMBP.local>
2016-11-05 16:26:37 -0700

    11/5/16

--- JavaScript.js
+++ JavaScript.js
@@ -17,24 +17,164 @@ var item = 'chair' //
 'desk' + item + 'dresser' // insert variables into strings
 'desk ${item} , dresser' // using ES6
 
-STRING METHODS
-item.toUpperCase(); //variable name to uppercase
-item.toLowerCase(); //variable name to lowercase
+STRING METHODS // point is to save work
+item.toUpperCase(); //variable name to uppercase (empty parentheses is what will run it, it will trigger/call it)
+item.toLowerCase(); //variable name to lowercase, you can put exception in parentheses so only for first two words
 item.slice(); // extracts a section of a string and returns a new string
 
 replace // finds match between regular expression and a string, replaces matched substrign with new substring
 var s = 'I like food'
 var re = /I/g
 var result = s.replace(re, 'we');
-document.write(result);
+document.write(result); // document.write is user sees what is typed, not a popup but shows up on page . console.log is only in chrome developer
 
 split // splits a string object into an array of strings
-var s = 'I like food'
-var ss = s.split(' ');
-for (var i in ss) {
-    document.write(ss[i]);
-    document.write('<br/>');
+var s = 'I like food';
+var array = s.split(' ');
+console.log(array);
+
+NUMBERS
+Integers // whole numbers , JavaScript is accurate to 15 digits
+Floats// decimals
+NaN // used to denote unrepresentable valeu like square - number. NaN is never equal to NaN
+
+OPERATORS
+-, +, *, /, %
+% // 100 % 1; tells if it is odd or even
+var num = 4;
+num = num + num; /*same as*/ num += num
+num = num * num /*same as*/ num *= num
+num++ // postfix increment
+var num = 5; // postix set the variable to a number
+y = num++; // define a new variable adding postfix, returns value before incementing
+console.log(num); //
+--num//prefix decrement
+
+RANDOM NUMBERS
+Math.random(); // get random deciman between 0 and 1
+Math.random() * 10; // random decimal between 0 and 10 (excluding 0 and 10)
+Math.floor(Math.random() * 11); // whole number between 0 and 10 including 0 and 10. Rounding down with math.floor
+Maath.floor(Math.random() * 10) + 1; // whole number between 1 and 10 rounding down
+Math.sqrt(Math.PI); // square pi
+Math.pow(5,4);// 5 to the power of 4
+
+
+UNDEFINED AND NULL
+undefined // uninitialized variable, no value assigned
+null // deliberate non-value. Can be used as a placeholder
+
+DATA TYPES
+typeof() // command allows you to check what data type dealing with
+parseInt('7') // converts 7 from string to a number & rounds
+(888).toString() // converts 888 int string '888'
+
+COERCION
+5 + 5 + 'j'; // + coerces numbers into strings
+5 * '55'; // * coerces string into numbers
+'10' / 2 // / coerces string into numbers
+'10' - 2 // - coerces string into numbers
+
+CONTROL FLOWS // if boolean expression within our condition is ture, a branch will execute, if it is false, it will not execute
+Conditionals // skip lines of code
+Loops // repeat lines of code
+Functions // save and later deploy lines of code
+
+BOOLEANS // true and false values. (binary values, transistors)
+true
+false
+! // not something ('bang')
+
+OPERATORS
+==,!=, === /*strict equality*/, !== // strict inequality
+>, <, >=, <= // relational operators
+&& (and), || (or)// are logical operators (comparing 2 sides of the equation not to each other but weather both sides evaluate to true)
+
+TRUTHINESS
+false, 0, ''(empty string), NaN, null, undefined
+Boolean('hi');// test it in the chrome console
+
+IF STATEMENT
+if (BOOLEAN EXPRESSION) {   //block start
+  // if boolean expression is true run this code
+}                           // block end
+
+PROMPT ASKING YOUR NAME
+prompt('What is your name'); // chrome will generate a popup
+var input = Prompt(); // save value of prompt to a variable
+console.log('hello' + input); // say hello
+if (input === 'andy') { // if the input is andy the code is run, otherwise the code is never run
+  console.log('andy is in');
 }
+
+IF, IF / ELSE IF / ELSE IF , ELSE
+if (input === 'andy') {....} // code in block will run if parentheses evaluate to true
+
+/*If else statements*/
+if (input === 'andy') {
+    console.log('admin login');
+} else {                         // it is a catch all
+   console.log('regular login');
+}
+
+/*else if statement*/
+var input = 'andy';
+
+if (input == 'andy') {
+  console.log('andy block');
+} else {
+  console.log('else block');
+}
+
+/* else if */
+var input = 'andy'
+if (input == 'andy') {
+  // run this piece of code
+} else if (input == 'steven') {
+  // run this piece of code
+} else {
+  console.log ('Else Block');
+}
+
+/*Buy Beer Example*/
+prompt('What is your age');
+var input = prompt( );
+
+if (input > 21) {
+	console.log('you can buy beer');
+} else if (input <= 0 ) {
+	console.log ("you don't exist");
+} else if (input == 2) {
+	console.log ('toddlers are not allowed');
+} else if (input < 9) {
+	console.log ('where are your parents');
+}
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
 
 
 
